13bbb5f5cd1b1242cfc5feb537de72c87049965e
Lionel Chanson <github@lionelchanson.fr>
2020-09-03 09:51:41 +0200

    Add a version number for pypi package
    Before this change version number in pypi is 2020.7.1 but installed package with pip is 0.0.0. The main goal is to have a correct version number for managing package update.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='pypi-slug',
+    version='0.1.0',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
